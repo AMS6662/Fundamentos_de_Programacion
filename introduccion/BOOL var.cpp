5e65c010b2634e48d9054430bee7918a82228b33
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-10-14 11:45:21 -0600

    Create BOOL var.cpp

---
+++ introduccion/BOOL var.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    bool BB = true;
+    cout << BB << endl;
+    cout << boolalpha; //Imprime true en lugar de 1
+    
+    //OPCIONES LÓGICAS (true, false)
+    // && -> and (disyunción) -> mujer AND <60
+    // || -> or (conjunción) -> kitkat OR snickers OR milkyway
+    // ! -> not -> NOT(mujer) = false
+
+    //Enteros
+    int n1 = 10;
+    int n2 = 20;
+
+    //Floats
+    float f1 = 1.5;
+    float f2 = 5.2;
+
+    //1. Igualdad
+    cout << "(n1 == n2): " << (n1 == n2) << endl;
+    cout << "(f1 == f2): " << (f1 == f2) << endl;
+
+    //2. Desigualdad
+    cout << "(n1 != n2):" << (n1 != n2) << endl;
+    cout << "(f1 != f2): " <<)
+
+
+}
