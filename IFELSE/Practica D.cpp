96648c9ad0e89884b3e2fd908f81526b9f694b18
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-11-04 12:41:49 -0600

    Update Practica D.cpp

--- IFELSE/Practica D.cpp
+++ IFELSE/Practica D.cpp
@@ -38,4 +38,56 @@ int main()
         cout << "El número mayour es " << C << endl;
     }
 
+/*1.17 Crear un programa que lea tres números diferentes, A, B, C, e imprime los valores
+máximo y mínimo. El procedimiento consistirá en comparaciones sucesivas de parejas de
+números. Cree el algoritmo, diagrama de flujo y código en C# */
+    int A, B, C;
+
+    cout << "Ingrese un número: ";
+    cin >> A;
+    cout << endl;
+
+    cout << "Ingrese otro número: ";
+    cin >> B;
+    cout << endl;
+
+    cout << "Ingrese otro número: ";
+    cin >> C;
+    cout << endl;
+
+    if (A > B and C < B)
+    {
+        cout << "El mínimo es " << C << " y el máximo es " << A;
+    }
+
+    if (B > A and C < A)
+    {
+        cout << "El mínimo es " << C << " y el máximo es " << B;
+    }
+
+    if (C > B and A < B)
+    {
+        cout << "El mínimo es " << A << " y el máximo es " << C; 
+    }
+
+    //B max A min
+    if (B > C and A < C)
+    {
+        cout << "El mínimo es " << A << " y el máximo es " << B;
+    }
+
+    //max A min B
+    if (A > C and B < C)
+    {
+        cout << "El mínimo es " << B << " y el máximo es " << A;
+    }
+
+    // MIN b MAX c
+    // 2, 1, 3
+    if (C > A and B < A)
+    {
+        cout << "El mínimo es " << B << " y el máximo es " << C;
+    }
+
+
 }
