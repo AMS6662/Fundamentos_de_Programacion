0b536a82ecbfe1ad3571b129b8e6a08e58eaa0b9
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-11-06 21:00:25 -0600

    Create Practica E.cpp

---
+++ instrucciones cicclicas/Practica E.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <locale>
+using namespace std;
+
+
+int main()
+{
+	/*1.21 Crear el código de un programa en C++ que pregunte si quiere salir del ciclo usando la
+instrucción “do … while”, y leer un “string” donde el usuario tiene solo dos opciones para
+contestar. Si el usuario contesta “SI” entonces salir del ciclo, su contesta “NO” entonces
+volver a hacer la pregunta “Quieres salir del ciclo? [SI/NO]”. Ojo la contestación deben ser
+estrictamente mayúsculas.*/
+	string respuesta = "NO";
+
+	do
+	{
+		cout << "Quieres salir del ciclo? [SI/NO]" << endl;
+		cin >> respuesta;
+	} while (respuesta != "SI");
+
+
+	/*1.22 Hacer un menú de opciones que ejecute las operaciones aritméticas básicas (+) suma,
+(-) resta, (*) multiplicación, (/) división y (%) residuo. que muestre un menú como el que
+se muestra en la figura de abajo. Deberá usar la instrucción “do … while”, y deberá leer un
+carácter y en base a la opción deberá ejecutar la operación, según la opción que elija.
+
+Los dos operandos deberán ser preguntados al usuario, convertirlos a “int” y guardados
+en dos variables con nombres “numero1” y “numero2”, el resultado debe ser guardado en
+una variable llamada “total”, el total debe ser impreso en la consola.
+
+Una vez que termine la operación, inicialice las variables “numero1”, “numero2” y “total”
+en cero y el usuario podrá hacer la siguiente operación, hasta que el usuario presione “s”
+de salir entonces terminará el programa.*/
+
+	char opciones = 'a';
+
+	do
+	{
+		cout << "Menu de operaciones" << endl;
+		cout << "a) Suma de dos operandos" << endl;
+		cout << "b) Resta de dos operandos" << endl;
+		cout << "c) Multiplicacion de dos operandos" << endl;
+		cout << "d) Division de dos operandos" << endl;
+		cout << "s) Salir del menu" << endl;
+		cin >> opciones;
+	} while (opciones != 's');
+
+
+
+
+
+
+}
