c6f32d34e28ff0a8ce034465d77a96fef1351bb0
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-11-11 13:31:50 -0600

    Update Practica F.cpp

--- instrucciones cicclicas/for/Practica F.cpp
+++ instrucciones cicclicas/for/Practica F.cpp
@@ -16,20 +16,44 @@ int main()
     {
         cout << limite << ", ";
     }
-    cout << "}";
+    cout << endl;
+    cout << endl;
 
 
     /*Generar el código necesario en C++ para contar los números de cero a 99. Tip : Necesitas inicializar el contador en uno y cambiar el limite a 99.*/
     int contador1 = 1;
 
-    for (int limite99 = 99 ; contador1 <= limite99 ; contador1++)
+    cout << "Los numero que hay antes del cien son ";
+
+    for (int limite99 = 99; contador1 <= limite99; contador1++)
     {
         cout << contador1 << ", ";
     }
-	    /*Generar el código necesario en C++ solicitando el rango uno mínimo y uno máximo para generar la impresión de los números de manera secuencial de uno en uno,
+    cout << endl;
+    cout << endl;
+
+    /*Generar el código necesario en C++ para contar los números del uno al cien pero solo imprimir los números pares. Tip : usa un “if” y usa la operación
+    residuo “%” para determinar que sea número par, y si es par, entonces imprime el número. Tip : revisa la solución del examen o las clases, cuando un
+    número era múltiplo de 3 o de 2.*/
+    int contador2 = 1;
+
+    cout << "Los numeros pares que existen entre uno y cien son: ";
+
+    for (int limite100 = 100; contador2 <= limite100; contador2++)
+    {
+        if (contador2 % 2 == 0)
+        {
+            cout << contador2 << ", ";
+        }
+    }
+    cout << endl;
+    cout << endl;
+
+
+    /*Generar el código necesario en C++ solicitando el rango uno mínimo y uno máximo para generar la impresión de los números de manera secuencial de uno en uno,
     si ingresa 1 como mínimo y 10 como máximo, deberá imprimir 1,2,3,4,5,6,7,8,9,10 Tip: Declara dos variables enteras (int) e insertalas en la instrucción for.*/
     int minimo, maximo;
-
+//BEING FIXED
     cout << "Ingrese el numero minimo: ";
     cin >> minimo;
     cout << endl;
