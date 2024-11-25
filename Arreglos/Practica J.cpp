b3cba9ac68979a416d580eac760d4d3754b9c7de
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-11-25 11:52:09 -0600

    Update Practica J.cpp

--- Arreglos/Practica J.cpp
+++ Arreglos/Practica J.cpp
@@ -28,4 +28,23 @@ int main()
 	{
 		cout << elementos << endl;
 	}
+
+
+/*#2 Realice el programa en C++ que encuentre el mayor elemento de un arreglo de tipo entero.
+Utilice el arreglo preconstruido*/
+	int numeros2[10]
+	{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+
+	vector <int> vnum
+	{ 34, 23, 94, 53, 16, 27, 5, 25, 42, 82 };
+
+	int mayor = -1;
+	for (int i = 0; i <= 9; i++)
+	{
+		if (vnum[i] > mayor)
+		{
+			mayor = vnum[i];
+		}
+	}
+	cout << "El mayor es: " << mayor << endl;
 }
