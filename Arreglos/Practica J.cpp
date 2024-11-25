b555a088c4e67c59db1a353793cd4014689f29c5
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-11-25 11:46:19 -0600

    Update Practica J.cpp

--- Arreglos/Practica J.cpp
+++ Arreglos/Practica J.cpp
@@ -9,26 +9,23 @@ int main()
    /*#1 Realice el programa en C++ que solicite un número e imprima el numero multiplicado por 10 y
    esto lo seguirá haciendo hasta que el usuario ingrese un cero. Utilice el arreglo preconstruido*/
 
-	int numeros[10];
-	int num, i = 0;
+	vector <int> numeros;
+	int num;
 	num = -1;
 
 	while (num != 0)
 	{
-		cout << "Ingrese un número: ";
+		cout << "Numero: ";
 		cin >> num;
-
-		if (num == 0 and i > 9)
+		if (num == 0)
 		{
 			break;
 		}
-		numeros[i] = num;
-		i++;
-
+		numeros.push_back(num);
+		cout << num * 10 << endl;
 	}
 	for (int elementos : numeros)
 	{
 		cout << elementos << endl;
 	}
-
 }
