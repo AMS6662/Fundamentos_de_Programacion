280c3ca3fdb61d4380ef48f7081b8728bd06083f
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-11-28 12:39:04 -0600

    Create Practica K.cpp

---
+++ Estructura de Datos/Practica K.cpp
@@ -0,0 +1,19 @@
+//DUE DEC 2
+#include <iostream>
+#include <limits>
+#include<iomanip>
+#include <locale>
+#include <vector>
+using namespace std;
+
+
+/*Practica K Funciones vence [Dic 2] Elabora un programa en C++ usando funciones , que calcule el area de las siguientes figuras geometricas:
+- Cuadrado
+- Rectangulo
+- Circulo
+- Triangulo*/
+
+int opcion;
+double pi = 3.1415, 
+
+
