371ff30e06b3cb27dd9da17a31100b9d0d7d613e
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-12-02 11:50:37 -0600

    Update and rename README.md to notas.cpp

--- Clases y Objetos/README.md
+++
@@ -1,8 +0,0 @@
-#Clases y Objetos
-
-##Clases
-Estructura o mapa de la infromación que contiene un objeto
-
-##Objetos
-Una instancia   de una clase
-*Encapsulación. Funciones y información son unidas
---
+++ Clases y Objetos/notas.cpp
@@ -0,0 +1,10 @@
+/Clases y Objetos
+
+Clases
+Estructura o mapa de la infromación que contiene un objeto
+
+Objetos
+Una instancia   de una clase
+- Encapsulación. Funciones y información son unidas*/
+
+  
