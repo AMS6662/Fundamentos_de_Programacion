cfb4cd6c475cb6bb676e050486b7769d3fe307b9
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-12-02 12:11:13 -0600

    Update notas.cpp

--- Clases y Objetos/notas.cpp
+++ Clases y Objetos/notas.cpp
@@ -6,16 +6,41 @@ Objetos
 Una instancia   de una clase
 - Encapsulación. Funciones y información son unidas*/
 
-
 #include <iostream>
 using namespace std;
 
 struct Alumno
 {
 	//Atributos o propiedades
+public:
 	string nombre;
 	int cuenta;
+
+	//Funciones de la estructura
+public:
+	static void altaAlumno()
+		{
+		//Tipo de dato -> Alumno alumno <- Nombre de la variable
+		Alumno alumno;
+		cout << "Ingrese su nombre: ";
+		cin >> alumno.nombre;
+
+		cout << "Ingrese su numero de cuenta: ";
+		cin >> alumno.cuenta;
+
+		imprimeDatos(alumno);
+
+		}
+
+	static void imprimeDatos(Alumno alumno)
+	{
+		cout << "Nombre: " << alumno.nombre << endl;
+		cout << "Cuenta: " << alumno.cuenta << endl;
+		cout << endl;
+	}
 };
+
+
 
 int main()
 {
@@ -35,5 +60,3 @@ int main()
 	cout << "Cuenta: " << Angela.cuenta << endl;
 
 }
-
-  
