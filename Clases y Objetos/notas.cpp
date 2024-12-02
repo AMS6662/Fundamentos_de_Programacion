c1562843d7cd2a7e3240f20c2aef410a7822a694
alex morales <alexander.morales35@my.unitec.edu.mx>
2024-12-02 11:55:41 -0600

    Update notas.cpp

--- Clases y Objetos/notas.cpp
+++ Clases y Objetos/notas.cpp
@@ -1,10 +1,39 @@
 /*Clases y Objetos
-
 Clases
 Estructura o mapa de la infromación que contiene un objeto
 
 Objetos
 Una instancia   de una clase
 - Encapsulación. Funciones y información son unidas*/
+
+
+#include <iostream>
+using namespace std;
+
+struct Alumno
+{
+	//Atributos o propiedades
+	string nombre;
+	int cuenta;
+};
+
+int main()
+{
+	Alumno Alex;
+	Alex.nombre = "Alex";
+	Alex.cuenta = 12345;
+
+	Alumno Angela;
+	Angela.nombre = "Angela";
+	Angela.cuenta = 67890;
+
+	cout << "Nombre: " << Alex.nombre << endl;
+	cout << "Cuenta: " << Alex.cuenta << endl;
+	cout << endl;
+
+	cout << "Nombre: " << Angela.nombre << endl;
+	cout << "Cuenta: " << Angela.cuenta << endl;
+
+}
 
   
